270684f2127adb88cf88d27f7cbb626e370716ad
nocdim <nokkudimu@gmail.com>
2022-01-06 19:18:39 +0300

    created dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Login'
+import Dashboard from './Dashboard'
+
+const code = new URLSearchParams(window.location.search).get('code')
 
 function App() {
   return (
