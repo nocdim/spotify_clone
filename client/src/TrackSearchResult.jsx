3cab307a66c6f0d539791c009851b987ec6f02b1
nocdim <nokkudimu@gmail.com>
2022-01-07 19:55:04 +0300

    song search done

--- client/src/TrackSearchResult.jsx
+++ client/src/TrackSearchResult.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const TrackSearchResult = () => {
+const TrackSearchResult = ({ track }) => {
     return (
-        <div>
-
+        <div className="d-flex m-2 align-items-center">
+            <img src={track.albumUrl} style={{height: '64px', width: '64px'}} />
+            <div className="m-3">
+                <div>{track.title}</div>
+                <div className="text-muted">{track.artist}</div>
+            </div>
         </div>
     )
 }
